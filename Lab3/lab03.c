87a877360801cb73e84cb1715c7ded6b9b823a54
Suphakon <suphakon143@gmail.com>
2025-07-15 18:50:52 +0700

    Add files via upload

--- Lab3/lab03.c
+++ Lab3/lab03.c
@@ -2,41 +2,33 @@
 #include <math.h>
 
 int isPrime(int num) {
-    if (num < 2) return 0;
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) return 0;
+    if (num < 2) return 0 ;
+    for (int i = 2; i <= sqrt(num) ; i++) {
+        if (num % i == 0) return 0 ;
     }
-    return 1;
+    return 1 ;
 }
-
 int main() {
-    int n;
-
-    printf("Enter N : ");
-    scanf("%d", &n);
-
-    int arr[n];
-
+    int n ;
+    printf("Enter N : ") ;
+    scanf("%d", &n) ;
+    int arr[n] ;
     for (int i = 0; i < n; i++) {
-        printf("Enter value[%d] : ", i);
-        scanf("%d", &arr[i]);
+        printf("Enter value[%d] : ", i) ;
+        scanf("%d", &arr[i]) ;
     }
-
-    printf("Index:  ");
+    printf(" Index:  ") ;
     for (int i = 0; i < n; i++) {
-        printf("%2d ", i);
+        printf(" %2d ", i) ;
     }
-    printf("\n");
-
-    printf("Array:  ");
+    printf("\n") ;
+    printf("Array:  ") ;
     for (int i = 0; i < n; i++) {
         if (isPrime(arr[i])) {
-            printf("%2d ", arr[i]);
+            printf("%2d ", arr[i]) ;
         } else {
-            printf("%2s ", "#");
+            printf("%2s ", "#") ;
         }
     }
-
-    printf("\n");
-    return 0;
-}
+    printf("\n") ;
+} // end function
